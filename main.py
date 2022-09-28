01cd92956c4109359b96d76299ecdfa7a7c2bc6e
ajinkya <ajinkya.dessai@analog.com>
2022-09-28 18:55:31 +0530

    Fitness data update complete

--- main.py
+++ main.py
@@ -30,10 +30,10 @@ category_dict = {
     }
     ,
     "Fitness_workout": {
-        "FitnessBlender": 'UCiP6wD_tYlYLYh3agzbByWQ',
+        # "FitnessBlender": 'UCiP6wD_tYlYLYh3agzbByWQ',
         # "blogilates": 'blogilates',
-        "XHIT_Daily": 'UCM1Nde-9eorUhq-teaWlgUA',
-        "POPSUGAR_Fitness": 'UCBINFWq52ShSgUFEoynfSwg',
+        # "XHIT_Daily": 'UCM1Nde-9eorUhq-teaWlgUA',
+        # "POPSUGAR_Fitness": 'UCBINFWq52ShSgUFEoynfSwg',
         "ScottHermanFitness": 'UCEtMRF1ywKMc4sf3EXYyDzw',
         "The_Fitness_Marshall": 'UCyqR7WkL8i1b6xtSssDmW9w',
         "Guru_Mann_Fitness": 'UCGMOauU8dOd4mv2bT3Tx57w',
